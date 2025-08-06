407a8b86ac3aa10bcb585e260646084aaba9dcae
diamondl53 <diamondl53@nycstudents.net>
2025-08-06 14:55:36 -0400

    first version

--- init-firebase.js
+++ init-firebase.js
@@ -2,7 +2,16 @@
  * @TODO replace the firebaseConfig object below with the one
  * provided for you by firebase after creating your project!
  */
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyDFhfcwUPDSfyi49nfDSi2AMXYWh25Wo8k",
+  authDomain: "fire-chat-17d62.firebaseapp.com",
+  databaseURL: "https://fire-chat-17d62-default-rtdb.firebaseio.com",
+  projectId: "fire-chat-17d62",
+  storageBucket: "fire-chat-17d62.firebasestorage.app",
+  messagingSenderId: "304101610061",
+  appId: "1:304101610061:web:d70a5a6fb4f8b7baf24204",
+  measurementId: "G-Q998SYEHTD"
+};
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
